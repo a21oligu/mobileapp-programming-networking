6cc41987ab1e6b680b31e56725c0181a8ab73955
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-26 19:01:24 +0200

    fetch, populate and display list of mountains fetched from api

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -7,18 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-@SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
-    ArrayList<MountainListItem> mountains = new ArrayList<>(Arrays.asList(new MountainListItem("Kebbekaise"),
-            new MountainListItem("Monte evereste"),
-            new MountainListItem("Omberg")));
+    ArrayList<MountainListItem> mountains = new ArrayList<>();
 
     private MountainAdapter mountainAdapter;
     private RecyclerView mountainView;
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         mountainAdapter = new MountainAdapter(this, mountains, new MountainAdapter.OnClickListener() {
             @Override
             public void onClick(MountainListItem mountain) {
-                Log.d("click-event", mountain.getTitle());
+                Log.d("click-event", mountain.getName());
             }
         });
 
@@ -40,15 +42,21 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         mountainView.setAdapter(mountainAdapter);
         System.out.println(mountainAdapter.getItemCount());
 
-        new JsonFile(this, this).execute(JSON_FILE);
-
-
-
+        JsonTask jsonTask = new JsonTask(this);
+        jsonTask.execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
-    }
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<MountainListItem>>() {}.getType();
 
+        Log.d("Response", String.format("Got response from GET: %b", json != null) );
+
+        ArrayList<MountainListItem> newMountains =  gson.fromJson(json, type);
+
+        mountains.addAll(newMountains);
+        mountainAdapter.notifyDataSetChanged();
+
+    }
 }
