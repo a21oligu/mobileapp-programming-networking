2d8a7af8cb509a2b0929c0711dcf6a9e09d7eaa1
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-26 15:53:51 +0200

    added recyclerview to activity_main

---
+++ app/src/main/java/com/example/networking/MountainAdapter.java
@@ -0,0 +1,46 @@
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.networking.R;
+
+public class MountainAdapter extends RecyclerView.Adapter<MountainAdapter.ViewHolder> {
+    private String[] localDataSet;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MountainAdapter.ViewHolder holder, int position) {
+        holder.getTextView().setText(localDataSet[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+
+}
