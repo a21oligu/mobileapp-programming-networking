158edb3d25724a720365d52a5fd458e79985cd34
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-26 17:03:40 +0200

    added a class representing a mountain

--- app/src/main/java/com/example/networking/MountainListItem.java
+++ app/src/main/java/com/example/networking/MountainListItem.java
@@ -1,2 +1,13 @@
-package com.example.networking;public class MountainListItem {
+package com.example.networking;
+
+public class MountainListItem {
+    private String title;
+
+    public MountainListItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
