2ed45081e2fd9f96b894d4bb4ea95ea37db91793
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-26 17:06:38 +0200

    implemented RecyclerView and adpater

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -11,14 +16,34 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
-    // Test commit
+    ArrayList<MountainListItem> mountains = new ArrayList<>(Arrays.asList(new MountainListItem("Kebbekaise"),
+            new MountainListItem("Monte evereste"),
+            new MountainListItem("Omberg")));
+
+    private MountainAdapter mountainAdapter;
+    private RecyclerView mountainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mountainAdapter = new MountainAdapter(this, mountains, new MountainAdapter.OnClickListener() {
+            @Override
+            public void onClick(MountainListItem mountain) {
+                Log.d("click-event", mountain.getTitle());
+            }
+        });
+
+        mountainView = findViewById(R.id.recycle_mountains);
+        mountainView.setLayoutManager(new LinearLayoutManager(this));
+        mountainView.setAdapter(mountainAdapter);
+        System.out.println(mountainAdapter.getItemCount());
+
         new JsonFile(this, this).execute(JSON_FILE);
+
+
+
     }
 
     @Override
